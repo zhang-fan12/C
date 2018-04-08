7fad2cd9df89ab44edfad3b3228927e4e052e67b
zhang-fan12 <37659193+zhang-fan12@users.noreply.github.com>
2018-04-08 21:02:48 +0800

    第一次作业
    第二题

---
+++ C语言第一次作业思考题/Project8/思考题.c
@@ -0,0 +1,15 @@
+#define	_CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int student = 500;
+
+void main()
+{
+	printf("\"How many student here\?\"\n");
+	printf("\"500\"");
+
+
+	printf("\n");
+
+	system("pause");
+}
